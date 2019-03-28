3e80771b760ebf89f324503f81b499a1b99b5570
kalariya-parikshith <k.parikshith93@gmail.com>
2019-03-28 15:04:34 +0530

    test storybook hosting

---
+++ storybook-static/precache-manifest.2ba07835ac71baffa2ee18a417db1554.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = [
+  {
+    "url": "main.902db97ecaa52d8a4234.bundle.js"
+  },
+  {
+    "url": "runtime~main.902db97ecaa52d8a4234.bundle.js"
+  },
+  {
+    "url": "vendors~main.902db97ecaa52d8a4234.bundle.js"
+  },
+  {
+    "revision": "e96f87ff6f5216c3fbe4c2554aa09731",
+    "url": "iframe.html"
+  }
+];
